58d730f50920c411bdfdc02cc3deae77037411d1
SureSRM <sergiozz95@gmail.com>
2019-04-19 11:21:24 +0200

    Add mocha chai

---
+++ src/trivial.test.js
@@ -0,0 +1,11 @@
+import { expect, assert } from 'chai'
+
+describe('Trivial test', () => {
+  it('Should complete ', () => {
+    expect(true)
+  })
+
+  xit('Should fail ', () => {
+    assert.fail()
+  })
+})
